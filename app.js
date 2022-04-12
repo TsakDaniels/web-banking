8c2b5345bb526b98ca8868acaa4e27ba2711c49b
GiorgosTsakonas <giorgos_tsakonas@outlook.com>
2022-04-12 22:29:46 +0300

    done iban

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
-let acc1 = new makeAccount("Johan", 1200, 000);
-let acc2 = new makeAccount("Eddie", 4000, 123);
-let acc3 = new makeAccount("Marie", 20000, 111);
+const acc1 = new makeAccount("Johan", 1200, 000, "0");
+const acc2 = new makeAccount("Eddie", 4000, 123, "0");
+const acc3 = new makeAccount("Marie", 20000, 111,"0");
 
 //prototype
 
-function makeAccount(owner, init, pass) {
+function makeAccount(owner, init, pass, iban) {
   this.name = owner;
   this.balance = init;
   this.password = pass;
+  this.iban = iban;
 }
 
-(makeAccount.prototype.withdraw = function (amount, pass) {
-  if (pass === this.password) {
-    if (Number.isInteger(amount) && Number.isInteger(this.balance)) {
-      if (this.balance - amount >= 0) {
-        this.balance = this.balance - amount;
-        return this.name + " your balance is " + this.balance;
+(makeAccount.prototype.getIban = function (iban) {
+  if (this.iban === "0") {
+  iban = Math.floor(100000 + Math.random() * 900000);
+  this.iban = "GR" + iban;
+  return this.iban;
+  } else {
+    return this.iban;
+  }
+})
+
+(
+  (makeAccount.prototype.withdraw = function (amount, pass) {
+    if (pass === this.password) {
+      if (Number.isInteger(amount) && Number.isInteger(this.balance)) {
+        if (this.balance - amount >= 0) {
+          this.balance = this.balance - amount;
+          return this.name + " your balance is " + this.balance;
+        } else {
+          return "Insufficient balance! " + this.name;
+        }
       } else {
-        return "Insufficient balance! " + this.name;
+        return "invalid amount";
       }
     } else {
-      return "invalid amount";
+      return "wrong password";
     }
-  } else {
-    return "wrong password";
-  }
-}),
+  })
+),
   (makeAccount.prototype.deposit = function (amount) {
     if (Number.isInteger(amount) && Number.isInteger(this.balance)) {
       this.balance = this.balance + amount;
@@ -37,5 +50,3 @@ function makeAccount(owner, init, pass) {
   (makeAccount.prototype.getBalance = function () {
     return this.name + "your balance is " + this.balance;
   });
-
-
